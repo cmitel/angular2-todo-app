0bf99606af3519ea00899e30d7ae6155387699c5
Chris MITEL <mitel.chris@gmail.com>
2017-02-08 15:45:10 +0100

    Importes the ShowTodoComponent in the RoutingModule

--- app/routing/routing.module.ts
+++ app/routing/routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ListComponent }        from '../todo/list.component';
 import { EditComponent }        from '../todo/edit.component';
+import { ShowTodoComponent }    from '../todo/show-todo.component';
 
 const appRoutes: Routes = [
     { path: '',          redirectTo: '/home', pathMatch: 'full' },
     { path: 'home',      component: ListComponent },
-    { path: 'edit/:id',  component: EditComponent }
+    { path: 'edit/:id',  component: EditComponent },
+    { path: 'view/:id',  component: ShowTodoComponent }
 ];
 
 @NgModule({
