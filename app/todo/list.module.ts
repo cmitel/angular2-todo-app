02760884c6876d983f9de14e25ce4efefe89d303
Chris MITEL <mitel.chris@gmail.com>
2017-02-08 15:44:26 +0100

    Adds the RouterModule to the ListModule for giving the ability to use RouterLink directive

--- app/todo/list.module.ts
+++ app/todo/list.module.ts
@@ -2,6 +2,7 @@ import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 import { FormsModule }      from '@angular/forms';
 import { HttpModule }       from '@angular/http';
+import { RouterModule }     from '@angular/router';
 
 import { ListComponent }    from './list.component';
 import { EditComponent }    from './edit.component';
@@ -10,7 +11,7 @@ import { ShowTodoComponent }    from './show-todo.component';
 import { TasksService }     from './tasks.service';
 
 @NgModule({
-    imports:         [ BrowserModule, FormsModule, HttpModule ],
+    imports:         [ BrowserModule, FormsModule, HttpModule, RouterModule ],
     exports:         [ ListComponent, EditComponent, ShowTodoComponent ],
     declarations:    [ ListComponent, EditComponent, ShowTodoComponent ],
     providers:       [ TasksService ]
