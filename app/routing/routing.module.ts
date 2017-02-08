fe49623a3898195107f037202861c73d3bf37093
Chris MITEL <mitel.chris@gmail.com>
2017-02-08 10:42:18 +0100

    Adds the EditComponent to the routing module

--- app/routing/routing.module.ts
+++ app/routing/routing.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ListComponent } from '../todo/list.component';
+import { ListComponent }        from '../todo/list.component';
+import { EditComponent }        from '../todo/edit.component';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home',  component: ListComponent }
+    { path: '',          redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home',      component: ListComponent },
+    { path: 'edit/:id',  component: EditComponent }
 ];
 
 @NgModule({
     imports: [ RouterModule.forRoot(appRoutes) ],
-    exports: [ RouterModule ]
+    exports: [ RouterModule ],
+    declarations: [],
+    providers: []
 })
-export class RoutingModule {}
+export class RoutingModule { }
