a6e01e75dd79f5dd321a9eab8639ef0e8f47e234
Chris MITEL <mitel.chris@gmail.com>
2017-02-08 10:39:35 +0100

    Adds the id number property to the task model

--- app/todo/task.ts
+++ app/todo/task.ts
@@ -1,12 +1,15 @@
 
 export class Task {
+
+    id: number;
     createDate: number;
     updateDate: number;
     description: string;
 
-    constructor(d: string) {
+    constructor(description: string, id: number) {
         this.createDate = Date.now();
         this.updateDate = Date.now();
-        this.description = d;
+        this.description = description;
+        this.id = id;
     }
 }
