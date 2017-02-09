35d7004c64cee56bbdc6aa63d4a6232756727f47
Chris MITEL <mitel.chris@gmail.com>
2017-02-09 17:02:38 +0100

    Adds the link to specific app component stlye

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'app.html',
+  styleUrls: [ 'app.css' ]
 })
 export class AppComponent implements OnInit { 
   name = 'Angular';  
